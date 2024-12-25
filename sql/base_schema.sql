0b497dad4d9ded87d0e9518317322484136d674e
BalramRajput <balramrajput777@gmail.com>
2024-12-25 16:52:39 +0530

    improved UI and backend modularization

--- sql/base_schema.sql
+++ sql/base_schema.sql
@@ -1,13 +1,51 @@
 -- create database upkaranamitra.db (sqlite3 upkaranmitra.db)
+
+CREATE TABLE `country` (
+    `country_id` INTEGER PRIMARY KEY,
+    `country_name` TEXT NOT NULL
+);
+
+CREATE TABLE `state` (
+    `state_id` INTEGER PRIMARY KEY,
+    `state_name` TEXT NOT NULL,
+    `country_id` INTEGER NOT NULL,
+    FOREIGN KEY (`country_id`) REFERENCES `country` (`country_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `district` (
+    `district_id` INTEGER PRIMARY KEY,
+    `district_name` TEXT NOT NULL,
+    `state_id` INTEGER NOT NULL,
+    FOREIGN KEY (`state_id`) REFERENCES `state` (`state_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `sub_district` (
+    `sub_district_id` INTEGER PRIMARY KEY,
+    `sub_district_name` TEXT NOT NULL,
+    `district_id` INTEGER NOT NULL,
+    FOREIGN KEY (`district_id`) REFERENCES `district` (`district_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `village` (
+    `village_id` INTEGER PRIMARY KEY,
+    `village_name` TEXT NOT NULL,
+    `sub_district_id` INTEGER NOT NULL,
+    FOREIGN KEY (`sub_district_id`) REFERENCES `sub_district` (`sub_district_id`) ON DELETE CASCADE
+);
+
+CREATE INDEX `idx_sub_district_id` ON `village`(`sub_district_id`);
+CREATE INDEX `idx_district_id` ON `sub_district`(`district_id`);
+CREATE INDEX `idx_state_id` ON `district`(`state_id`);
+CREATE INDEX `idx_country_id` ON `state`(`country_id`);
+
+
 CREATE TABLE `address` (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
     `address_line1` TEXT NOT NULL,
     `address_line2` TEXT,
-    `village` TEXT NOT NULL,
-    `city` TEXT NOT NULL,
-    `state` TEXT NOT NULL,
-    `country` TEXT NOT NULL,
-    `zipcode` INTEGER NOT NULL
+    `village_id` INTEGER NOT NULL,
+    `zipcode` INTEGER NOT NULL,
+    FOREIGN KEY (`village_id`) REFERENCES `village`(`village_id`)
 );
 
 CREATE TABLE `user`(
